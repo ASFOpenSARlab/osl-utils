fb50fd84a1d286476766182400dedfe2821ff1f6
Cameron Showalter <cjshowalter@alaska.edu>
2025-11-07 11:11:28 -0900

    Finally fixed the pathing issue

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -5,9 +5,14 @@ import relativeLinksRule from "markdownlint-rule-relative-links"
 const config = {
   config: {
     default: true,
-    "relative-links": "/code",
+    "relative-links": {
+      // The root of the project for resolving paths,
+      // NOT the path to start looking for md's from.
+      root_path: "/code",
+    },
   },
-  globs: ["**/*.md"],
+  // HERE is where you specify the path to files to lint:
+  globs: ["/code/**/*.md"],
   ignores: [
     "**/node_modules",
   ],
